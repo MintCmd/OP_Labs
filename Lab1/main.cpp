5d1d651cf69e0ed3521d4ddc942bad7be958ddc5
MintCmd <144525573+MintCmd@users.noreply.github.com>
2024-09-21 10:23:07 +0200

    Fix: теперь в printTypeInfo не нужен аргумент

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -14,13 +14,13 @@
 // - double, 8 байтов, 15-16 цифр 
 // 4. Логический тип
 // - bool, 1 байт, true/false
-// typeName == "char" || typeName == "unsigned char"
+
 #include <iostream>
 #include <limits>
 #include <typeinfo>
 
 template<typename T>
-void printTypeInfo(const std::string& typeName)
+void printTypeInfo()
 {
 	if (std::is_same<T, char>::value || std::is_same<T, unsigned char>::value) {
 		std::cout << typeid(T).name() << ", " << sizeof(T) << " bytes, " << static_cast<int>(std::numeric_limits<T>::min()) << "..." << static_cast<int>(std::numeric_limits<T>::max()) << std::endl;
@@ -32,29 +32,30 @@ void printTypeInfo(const std::string& typeName)
 
 int main() 
 {
-	std::cout << "Data types:" << std::endl;
+	std::cout << "Data types:" << std::endl; // Типы данных
 
-	std::cout << std::endl << "Integer types:" << std::endl;
-	printTypeInfo<char>("char");
-	printTypeInfo<unsigned char>("unsigned char");
-	printTypeInfo<short>("short");
-	printTypeInfo<unsigned short>("unsigned short");
-	printTypeInfo<int>("int");
-	printTypeInfo<unsigned int>("unsigned int");
-	printTypeInfo<long long>("long long");
-	printTypeInfo<unsigned long long>("unsigned long long");
+	std::cout << std::endl << "Integer types:" << std::endl; // Целочисленные
+	printTypeInfo<char>();
+	printTypeInfo<unsigned char>();
+	printTypeInfo<short>();
+	printTypeInfo<unsigned short>();
+	printTypeInfo<int>();
+	printTypeInfo<unsigned int>();
+	printTypeInfo<long long>();
+	printTypeInfo<unsigned long long>();
+	std::cout << "__int64 is long long" << std::endl;
 
-	std::cout << std::endl << "Character types:" << std::endl;
-	printTypeInfo<char>("char");
+	std::cout << std::endl << "Character types:" << std::endl; // Символьные
+	printTypeInfo<char>();
 
-	std::cout << std::endl << "Real types:" << std::endl;
-	printTypeInfo<float>("float");
-	printTypeInfo<double>("double");
+	std::cout << std::endl << "Real types:" << std::endl; // Вещественные
+	printTypeInfo<float>();
+	printTypeInfo<double>();
 
-	std::cout << std::endl << "Logical type:" << std::endl;
-	printTypeInfo<bool>("bool");
+	std::cout << std::endl << "Logical type:" << std::endl; // Логический
+	printTypeInfo<bool>();
 
-	std::cout << std::endl <<"Arithmetic operations:" << std::endl;
+	std::cout << std::endl <<"Arithmetic operations:" << std::endl; // Арифметические операции
 
 	char c = 10, h = 15;
 	std::cout << c - h << std::endl; // -5
@@ -62,8 +63,8 @@ int main()
 	short s = 500, o = 20;
 	std::cout << s * o << std::endl; // 10000
 
-	int a = 25, b = 10;
-	std::cout << a + b << std::endl; // 35
+	int a = 74572, b = 100;
+	std::cout << a / b << std::endl; // 745
 
 	char m = 'A', n = 'B';
 	std::cout << m + n << std::endl; // 131
