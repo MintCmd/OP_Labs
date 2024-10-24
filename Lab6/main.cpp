c64dccad8cbb52d6301add66580171d34b8d447b
MintCmd <144525573+MintCmd@users.noreply.github.com>
2024-10-24 15:11:13 +0200

    upd: синтаксис и разбивка на зоны видимости

--- Lab6/main.cpp
+++ Lab6/main.cpp
@@ -16,241 +16,209 @@
 а среди оставшихся продублировать простые числа.
 */
 
-
+// Все задачи расположены в разных зонах видимости, поэтому можно использовать одинаковые имена переменнных
+// и ошибки переопределения не будет и можно не создавать n, n2, n3, seq1[], seq2[], seq3[] и т.д.
 #include <iostream>
 
-int main()
-{
+int main() {
     //Задание 1.
-    const int N_Max = 10000;
-    int seq1[N_Max];
-    bool hasPrime = false;
-    int n;
-    std::cout << "Task 1." << std::endl << "Input: n: ";
-    std::cin >> n;
-    std::cout << "Input: " << n << " values: ";
-    for(int i=0; i<n; i++) //Цикл для ввода последовательности
     {
-        std::cin >> seq1[i];
-    }
-        
-    for(int i=0; i<n; i++) //Цикл для проверки, есть ли в последовательности проcтое число
-    {
-        bool isPrime=true;
-        for(int d=2; d<=sqrt(seq1[i]); d++)
-        {
-            if(seq1[i]%d == 0)
-            {
-                isPrime = false;
+        const int N_Max = 10000;
+        int seq[N_Max];
+        bool hasPrime = false;
+        int n;
+        std::cout << "Task 1." << std::endl << "Input: n: ";
+        std::cin >> n;
+        std::cout << "Input: " << n << " values: ";
+        for(int i=0; i<n; i++) { //Цикл для ввода последовательности
+            std::cin >> seq[i];
+        }
+            
+        for(int i=0; i<n; i++) { //Цикл для проверки, есть ли в последовательности проcтое число
+            bool isPrime=true;
+            for(int d=2; d<=sqrt(seq[i]); d++) {
+                if(seq[i]%d == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime) {
+                hasPrime=true;
                 break;
             }
         }
-        if(isPrime)
-        {
-            hasPrime=true;
-            break;
+        if(hasPrime) {
+            std::cout << "It has a prime number." << std::endl;
         }
-    }
-    if(hasPrime)
-    {
-        std::cout << "It has a prime number." << std::endl;
-    }
-    else
-    {
-        for(int i=0; i<n-1; i++) //Двойной цикл для сортировки по невозрастанию SWAP SORT
-        {
-            for(int j=i+1; j<n; j++)
-            {
-                if(seq1[i] < seq1[j])
-                {
-                    int tmp = seq1[i];
-                    seq1[i] = seq1[j];
-                    seq1[j] = tmp;
+        else {
+            for(int i=0; i<n-1; i++) { //Двойной цикл для сортировки по невозрастанию SWAP SORT
+                for(int j=i+1; j<n; j++) {
+                    if(seq[i] < seq[j]) {
+                        int tmp = seq[i];
+                        seq[i] = seq[j];
+                        seq[j] = tmp;
+                    }
                 }
             }
-        }
-        std::cout << "Sorted sequance: ";
-        for(int i = 0; i<n; i++)
-        {
-            std::cout << seq1[i] << " ";
-        }
-    }
-
-    //Задание 2.
-    int seq2[N_Max];
-    int firstDigit[N_Max];
-    int minDigit[N_Max];
-    int n2;
-    std::cout << std::endl << "Task 2." << std::endl << "Input: n: ";
-    std::cin >> n2;
-    std::cout << "Input: " << n2 << " values: ";
-    for(int i=0; i<n2; i++) //Цикл для ввода массива и поиска первой и минимальной цифр
-    {
-        std::cin >> seq2[i];
-        firstDigit[i] = seq2[i];
-
-        while(firstDigit[i] > 9)
-        {
-            firstDigit[i] /= 10;
-        } 
-        int dif = seq2[i];
-        minDigit[i] = 9;
-
-        while(dif > 0)
-        {
-            int min = dif%10;
-            if(minDigit[i] < min)
-            {
-                minDigit[i] = min;
+            std::cout << "Sorted sequance: ";
+            for(int i = 0; i<n; i++) {
+                std::cout << seq[i] << " ";
             }
-            dif /= 10;
         }
-    }
-
-    for(int i=0; i<n2-1; i++) //Двойной цикл для сортировки по неубыванию
-    {
-        for(int j=i+1; j<n2; j++)
-        {
-            if((firstDigit[i] > firstDigit[j]) || 
-                ((firstDigit[i] == firstDigit[j]) && (minDigit[i] > minDigit[ j])) || 
-                    ((firstDigit[i] == firstDigit[j]) && (minDigit[i] == minDigit[j]) && (seq2[i] > seq2[j])))
-            {
-                int tmp = seq2[i];
-                seq2[i] = seq2[j];
-                seq2[j] = tmp;
-
-                tmp = firstDigit[i];
-                firstDigit[i] = firstDigit[j];
-                firstDigit[j] = tmp;
-
-                tmp = minDigit[i];
-                minDigit[i] = minDigit[j];
-                minDigit[j] = tmp;
-            }
-        }        
-    } 
-            
-    std::cout << "Sorted sequance: ";
-    for(int i = 0; i<n2; i++)
-    {
-        std::cout << seq2[i] << " ";
     }
     
 
-    //Задание 3.
-    const int N_Max3 = 100, M_Max3 = 100;
-    int matrix[N_Max3][M_Max3];
-    int n3, m3;
-    int sumstr = INT_MAX;
-    int istr;
-    std::cout << std::endl << "Task 3." << std::endl << "Input: n and m: ";
-    std::cin >> n3;
-    std::cin >> m3;
-    std::cout << "Input: " << n3 << "x" << m3 << " matrix values below." << std::endl;
-    for(int i=0; i<n3; i++) //Цикл для ввода последовательности
+    //Задание 2.
     {
-        for(int j=0; j<m3; j++)
-        {
-            std::cin >> matrix[i][j];
-        }
-    }
+        const int N_Max = 10000;
+        int seq[N_Max];
+        int firstDigit[N_Max];
+        int minDigit[N_Max];
+        int n;
+        std::cout << std::endl << "Task 2." << std::endl << "Input: n: ";
+        std::cin >> n;
+        std::cout << "Input: " << n << " values: ";
+        for(int i=0; i<n; i++) { //Цикл для ввода массива и поиска первой и минимальной цифр
+            std::cin >> seq[i];
+            firstDigit[i] = seq[i];
 
-    for(int i=0; i<n3; i++) //Цикл для нахождения минимальной суммы строки и номера строки
-    {
-        int minsum = 0;
-        for(int j=0; j<m3; j++)
-        {
-            minsum += matrix[i][j];
-        }
-        if(sumstr > minsum)
-        {
-            sumstr = minsum;
-            istr = i;
-        }    
-    }
-    for(int i=0; i<n3; i++) //Цикл для замены значений строки на сумму
-    {   
-        if(istr == i)
-        {
-            for(int j=0; j<m3; j++)
-            {
-                matrix[i][j] = sumstr;
+            while(firstDigit[i] > 9) {
+                firstDigit[i] /= 10;
+            } 
+            int dif = seq[i];
+            minDigit[i] = 9;
+
+            while(dif > 0) {
+                int min = dif%10;
+                if(minDigit[i] < min) {
+                    minDigit[i] = min;
+                }
+                dif /= 10;
             }
         }
-    }
-    std::cout << "Changed matrix:" << std::endl;
-    for(int i=0; i<n3; i++) //Цикл для вывода изменённого массива
-    {
-        for(int j=0; j<m3; j++)
-        {
-            std::cout << matrix[i][j] << " ";
+
+        for(int i=0; i<n-1; i++) { //Двойной цикл для сортировки по неубыванию 
+            for(int j=i+1; j<n; j++) {
+                if((firstDigit[i] > firstDigit[j]) || 
+                    ((firstDigit[i] == firstDigit[j]) && (minDigit[i] > minDigit[ j])) || 
+                        ((firstDigit[i] == firstDigit[j]) && (minDigit[i] == minDigit[j]) && (seq[i] > seq[j]))) {
+                    int tmp = seq[i];
+                    seq[i] = seq[j];
+                    seq[j] = tmp;
+
+                    tmp = firstDigit[i];
+                    firstDigit[i] = firstDigit[j];
+                    firstDigit[j] = tmp;
+
+                    tmp = minDigit[i];
+                    minDigit[i] = minDigit[j];
+                    minDigit[j] = tmp;
+                }
+            }        
+        } 
+                
+        std::cout << "Sorted sequance: ";
+        for(int i = 0; i<n; i++) {
+            std::cout << seq[i] << " ";
         }
-        std::cout << std::endl;
     }
 
+    //Задание 3.
+    {
+        const int N_Max = 100, M_Max = 100;
+        int matrix[N_Max][M_Max];
+        int n, m;
+        int sumstr = INT_MAX;
+        int istr;
+        std::cout << std::endl << "Task 3." << std::endl << "Input: n and m: ";
+        std::cin >> n;
+        std::cin >> m;
+        std::cout << "Input: " << n << "x" << m << " matrix values below." << std::endl;
+        for(int i=0; i<n; i++) { //Цикл для ввода последовательности 
+            for(int j=0; j<m; j++) {
+                std::cin >> matrix[i][j];
+            }
+        }
+
+        for(int i=0; i<n; i++) { //Цикл для нахождения минимальной суммы строки и номера строки
+            int minsum = 0;
+            for(int j=0; j<m; j++) {
+                minsum += matrix[i][j];
+            }
+            if(sumstr > minsum) {
+                sumstr = minsum;
+                istr = i;
+            }    
+        }
+        for(int i=0; i<n; i++) { //Цикл для замены значений строки на сумму
+            if(istr == i) {
+                for(int j=0; j<m; j++) {
+                    matrix[i][j] = sumstr;
+                }
+            }
+        }
+        std::cout << "Changed matrix:" << std::endl;
+        for(int i=0; i<n; i++) { //Цикл для вывода изменённого массива
+            for(int j=0; j<m; j++) {
+                std::cout << matrix[i][j] << " ";
+            }
+            std::cout << std::endl;
+        }
+    }
 
     //Задание 4.
-    const int N_Max20k = 20000;
-    int seq4[N_Max20k];
-    int n4;
-    std::cout << std::endl << "Task 4." << std::endl << "Input: n: ";
-    std::cin >> n4;
-    std::cout << "Input: " << n4 << " values: ";
-    for(int i=0; i<n4; i++) //Цикл для ввода последовательности
     {
-        std::cin >> seq4[i];
-    }
-        
-    int j1 = 0; //Удаление
-    for(int i=0;i<n4;i++)
-    {
-        int y = 0;
-        int x = seq4[i];
-        while(x>0) 
-        {
-            int d = x % 10;
-            y = y*10 + d;
-            x = x/10;
+        const int N_Max = 20000;
+        int seq[N_Max];
+        int n;
+        std::cout << std::endl << "Task 4." << std::endl << "Input: n: ";
+        std::cin >> n;
+        std::cout << "Input: " << n << " values: ";
+        for(int i=0; i<n; i++) { //Цикл для ввода последовательности
+            std::cin >> seq[i];
         }
-        if (seq4[i] == y)
-        {
-            continue;
+            
+        int j1 = 0; //Удаление
+        for(int i=0;i<n;i++) {
+            int y = 0;
+            int x = seq[i];
+            while(x>0) {
+                int d = x % 10;
+                y = y*10 + d;
+                x = x/10;
+            }
+            if (seq[i] == y) {
+                continue;
+            }
+            seq[j1] = seq[i];
+            j1++;
         }
-        seq4[j1] = seq4[i];
-        j1++;
-    }
-    n4 = j1;
-    std::cout << "Sequence with deleted palindromes: ";
-    for(int i = 0; i<n4; i++)
-    {
-        std::cout << seq4[i] << " ";  
-    }     
+        n = j1;
+        std::cout << "Sequence with deleted palindromes: ";
+        for(int i = 0; i<n; i++) {
+            std::cout << seq[i] << " ";  
+        }     
 
-    for(int i=0;i<n4;i++) //Дублирование простых чисел
-    {    
-        bool isPrime=true;
-        for(int d=2; d<=sqrt(seq4[i]); d++)
-        {
-            if(seq4[i]%d == 0)
-            {
-                isPrime = false;
-                break;
+        for(int i=0;i<n;i++) { //Дублирование простых чисел
+            bool isPrime=true;
+            for(int d=2; d<=sqrt(seq[i]); d++) {
+                if(seq[i]%d == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime) {
+                for(int j=n;j>1;j--) {
+                    seq[j] = seq[j-1];
+                }
+                i++;
+                n++;
             }
         }
-        if(isPrime)
-        {
-            for(int j=n;j>1;j--)
-            {
-                seq4[j] = seq4[j-1];
-            }
-            i++;
-            n4++;
+        std::cout << std::endl << "Sequence with doubled prime numbers: ";
+        for(int i = 0; i<n; i++) {
+            std::cout << seq[i] << " ";
         }
-    }
-    std::cout << std::endl << "Sequence with doubled prime numbers: ";
-    for(int i = 0; i<n4; i++)
-    {
-        std::cout << seq4[i] << " ";
     }
     return 0;
 }
